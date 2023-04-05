629eef9050de7872d57269a73d316b081edcf9de
wdai <wenhong.dai@cellsignal.com>
2023-04-05 14:42:01 -0400

    change to hello gitops

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprint(w, "Hello, World!")
+        fmt.Fprint(w, "Hello, GitOps!")
     })
 
     http.ListenAndServe(":8080", nil)
